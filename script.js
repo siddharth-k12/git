b5395993201ac09687f6a6bb09fe6c33e20e257e
Siddharth <siddharthkumar00021@gmail.com>
2025-07-27 19:39:05 +0530

    second js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let b = 20;
 function add() {
     return a + b;
 }
+function subtract() {
+    return a - b;
+}   
+function multiply() {
+    return a * b;
+}   
+function divide() {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}   
